aa663e217e194541d6e72cc42e5104b8404c5ef3
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2022-01-17 10:28:43 +0900

    add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+let elenvelope = document.querySelector('.menu-btn');
+
+elenvelopetop = document.querySelector('.menu');
+
+// elpaper = document.querySelector('.paper')
+
+elenvelope.addEventListener('click',function(e){
+elenvelopetop.classList.toggle('menu-active');
+// elpaper.classList.toggle('paper-close');
+
+});
